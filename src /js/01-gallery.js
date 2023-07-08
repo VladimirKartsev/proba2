6883b47bcc74bffd38c2ef948c551345f198e69e
Vladimir <kartcev2204@gmail.com>
2023-07-08 22:16:16 +0300

    Update 01-gallery.js

--- src /js/01-gallery.js
+++ src /js/01-gallery.js
@@ -2,26 +2,4 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const galleryList = document.querySelector('.gallery');
-const markup = createGalleryItems(galleryItems);
-
-galleryList.insertAdjacentHTML('beforeend', markup);
-function createGalleryItems(items) {
-  return items
-    .map(({ preview, original, description }) => {
-      return `<li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>`;
-    })
-    .join('');
-}
-galleryList.Style.listStyle = 'none';
-var lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: '250',
-});
-
 console.log(galleryItems);
